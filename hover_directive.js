f54f82b84f74ff0914a304bddc0f3f3e82fcae90
Danny Emmerson <danny.emmerson911@gmail.com>
2018-02-17 15:03:08 -0500

    directive fix

--- hover_directive.js
+++ hover_directive.js
@@ -1,14 +1,17 @@
 (function(){
-	return {
-		link: function($scope, $element, $attrs){
-				$element.on("mouseenter", function(){
-					$element.css("background-color", "#bbb");
-				};
-				$element.on("mouseout", function(){
-					$element.css("background-color" "#eee");
-				}
-			};
-		};	
+	function hoverState(){
+        return {
+    		link: function($scope, $element, $attrs){
+    				$element.on("mouseover", function(){
+    					$element.css('background-color', '#bbb');
+                        $element.css('border-radius', '7px');
+    				});
+    				$element.on("mouseout", function(){
+    					$element.css("background-color", '#fff');
+    				});
+    			}
+    		};	
+    }
 angular
 	.module("AppModule")
 	.directive("hoverState", hoverState);
